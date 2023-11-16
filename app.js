c3c44b3fc8b2c2025038e4bbf2195cf8141a2bbe
Josu-A <aginagajosu@gmail.com>
2023-11-16 18:26:44 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const createError = require('http-errors');
+const express = require("express");
+const path = require('path');
+const session = require('express-session');
+const logger = require('morgan');
+
+const indexRouter = require('./router/index');
+
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sess = {
+    secret: 'ausazko hitz multzoa',
+    cookie: {}
+}
+app.use(session(sess))
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+// Access the session as req.session
+app.get('/', function(req, res, next) {
+    if (req.session.views) {
+        req.session.views++
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<p>views: ' + req.session.views + '</p>')
+        res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+        res.end()
+    } else {
+        req.session.views = 1
+        res.end('welcome to the session demo. refresh!')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
