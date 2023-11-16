a78d94fa9a1c1d8e9aec6da1f3ac7c501a4ee726
Josu-A <aginagajosu@gmail.com>
2023-11-16 20:44:46 +0100

    Add default routing

--- app.js
+++ app.js
@@ -42,21 +42,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-// Access the session as req.session
-app.get('/', function(req, res, next) {
-    if (req.session.views) {
-        req.session.views++
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<p>views: ' + req.session.views + '</p>')
-        res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-        res.end()
-    } else {
-        req.session.views = 1
-        res.end('welcome to the session demo. refresh!')
-    }
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
